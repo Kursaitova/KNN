ed97acb1870938c4cad54a30383d042131481cb4
Kursaitova <32087142+Kursaitova@users.noreply.github.com>
2017-10-28 19:48:17 +0300

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -7,10 +7,8 @@ from sklearn import neighbors, datasets
 
 n_neighbors = 15
 
-# импорт данных
 iris = datasets.load_iris()
 
-# используетс€ только 2 признака
 X = iris.data[:, :2]
 y = iris.target
 
@@ -25,24 +23,19 @@ cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
 cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 for weights in ['uniform', 'distance']:
-    # создаетс€ экземпл€р  лассификатора соседей и подстраиваетс€ под данные
     clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
     clf.fit(X, y)
 
-    # участок границы решени€
-    # дл€ этого каждой точке сетки [x_min, x_max]x[y_min, y_max] присваиваютс€ цвета
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
-    # помещает результат в цветовую схему
     Z = Z.reshape(xx.shape)
     plt.figure()
     plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
 
-    # также рисуютс€ учебные точки
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
                 edgecolor='k', s=20)
     plt.xlim(xx.min(), xx.max())
